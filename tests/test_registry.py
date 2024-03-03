e007452752536e10770ddc75ce5c093e223812e3
0xkorin <0xkorin@proton.me>
2024-03-03 12:54:00 +0100

    Registry tests

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -1,6 +1,6 @@
 from ape import reverts
 from pytest import fixture
-from _constants import ZERO_ADDRESS
+from _constants import *
 
 @fixture
 def registrar(accounts):
@@ -22,6 +22,7 @@ def gauge(project, deployer, ygauge):
     return project.MockGauge.deploy(ygauge, ZERO_ADDRESS, sender=deployer)
 
 def test_register(registrar, registry, ygauge, gauge):
+    # register a gauge
     assert registry.num_gauges() == 0
     assert registry.ygauges(0) == ZERO_ADDRESS
     with reverts():
@@ -34,6 +35,7 @@ def test_register(registrar, registry, ygauge, gauge):
     assert registry.gauge_map(ygauge) == gauge
 
 def test_register_permission(deployer, registry, gauge):
+    # only registrar can register a gauge
     with reverts():
         registry.register(gauge, sender=deployer)
 
@@ -51,6 +53,7 @@ def test_register_double(project, deployer, registrar, registry, ygauge, gauge):
         registry.register(gauge2, sender=registrar)
 
 def test_deregister(project, deployer, registrar, registry, ygauge, gauge):
+    # deregister a gauge
     ygauge2 = project.MockYearnGauge.deploy(sender=deployer)
     gauge2 = project.MockGauge.deploy(ygauge2, ZERO_ADDRESS, sender=deployer)
     registry.register(gauge, sender=registrar)
@@ -87,6 +90,7 @@ def test_deregister_last(project, deployer, registrar, registry, ygauge, gauge):
     assert registry.gauge_map(ygauge2) == ZERO_ADDRESS
 
 def test_deregister_permission(registrar, registry, gauge):
+    # only management can deregister a gauge
     registry.register(gauge, sender=registrar)
     with reverts():
         registry.deregister(gauge, 0, sender=registrar)
@@ -116,15 +120,18 @@ def test_deregister_idx(project, deployer, registrar, registry, gauge):
         registry.deregister(gauge, 1, sender=deployer)
 
 def test_set_registrar(deployer, alice, registrar, registry):
+    # set new registrar address
     assert registry.registrar() == registrar
     registry.set_registrar(alice, sender=deployer)
     assert registry.registrar() == alice
 
 def test_set_registrar_permission(alice, registry):
+    # only management can set new registrar
     with reverts():
         registry.set_registrar(alice, sender=alice)
 
 def test_set_management(deployer, alice, registry):
+    # management can propose a replacement
     assert registry.management() == deployer
     assert registry.pending_management() == ZERO_ADDRESS
     registry.set_management(alice, sender=deployer)
@@ -132,16 +139,19 @@ def test_set_management(deployer, alice, registry):
     assert registry.pending_management() == alice
 
 def test_set_management_undo(deployer, alice, registry):
+    # proposed replacement can be undone
     registry.set_management(alice, sender=deployer)
     registry.set_management(ZERO_ADDRESS, sender=deployer)
     assert registry.management() == deployer
     assert registry.pending_management() == ZERO_ADDRESS
 
 def test_set_management_permission(alice, registry):
+    # only management can propose a replacement
     with reverts():
         registry.set_management(alice, sender=alice)
 
 def test_accept_management(deployer, alice, registry):
+    # replacement can accept management role
     registry.set_management(alice, sender=deployer)
     registry.accept_management(sender=alice)
     assert registry.management() == alice
